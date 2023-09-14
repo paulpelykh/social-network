9697f792f0731a37472c154fba4fa3e71fbf82ac
Paul <eeddll1505@gmail.com>
2023-09-14 18:12:50 +0300

    ended back-end of app

--- server/index.js
+++ server/index.js
@@ -8,7 +8,15 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { register } from 'controllers/auth';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/user.js';
+import postRoutes from './routes/posts.js';
+import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,6 +46,11 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost),
+  /* ROUTES */
+  app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
@@ -48,5 +61,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // Add data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch(err => console.error(`${err} did not connect`));
